63edfb63a80f673853f2cf383afe3f04b0d43508
blutuu <7216997+blutuu@users.noreply.github.com>
2018-09-14 16:34:39 -0400

    Minor script update.
    Beginning card selection functions.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-	
 
-	
+	// Adding 'active' class to the clicked card.
+	$(".oe_card").find('.ben_logo').on('click', function(){
+		$(this).toggleClass('active');
+	});
+
+
 });
