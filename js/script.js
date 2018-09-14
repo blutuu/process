3de53bdf16d9e761fa0c452b272121209b79b00d
blutuu <7216997+blutuu@users.noreply.github.com>
2018-09-14 10:05:15 -0400

    Updated Sass and Jade
    Added a benefits link to index.jade.
    Modified benefits.jade card styles.

--- js/script.js
+++ js/script.js
@@ -1,34 +1,5 @@
-var mainBody = document.querySelector('.main'); // parent element
-var newElement; // child template element
-var colors = ['#FF6633', '#FFB399', '#FF33FF', '#6680B3', '#66991A','#66994D'];
+$(document).ready(function() {
+	
 
-
-function makeBoxes(count) {
-
-	for (var i = 0; i < count; i++) {
-		newElement = document.createElement('div');
-		newElement.className = 'box';
-		newElement.style.backgroundColor = colors[i%colors.length];
-		mainBody.appendChild(newElement);
-	}
-
-	mainBody.addEventListener('click', function(e) {
-		e.target.parentNode.removeChild(e.target);
-	}, false);
-}
-
-
-function countUp() {
-	var number = 0;
-
-	function increment() {
-		return console.log(number++);
-	}
-
-	increment();
-	increment();
-	increment();
-	increment();
-}
-
-countUp();
+	
+});
