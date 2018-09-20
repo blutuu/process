b06e9488e6d580fa4af937c1c21fc29ccbad6ae3
blutuu <7216997+blutuu@users.noreply.github.com>
2018-09-20 16:33:03 -0400

    Added selection animation.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function() {
 
 	// Adding 'active' class to the clicked card.
-	$(".oe_card").find('.ben_logo').on('click', function(){
-		$(this).toggleClass('active');
+	$('.oe_card').find('.ben_logo').on('click', function(){
+		$('.oe_card').removeClass('active');
+		$(this).parent('.oe_card').toggleClass('active');
 	});
 
 
