cdc4fdbe9791a688e88df4b85a2632b7acd4ec36
blutuu <7216997+blutuu@users.noreply.github.com>
2018-09-21 14:29:54 -0400

    Updated grid and more modal.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
 $(document).ready(function() {
 
-	// Adding 'active' class to the clicked card.
-	$('.oe_card').find('.ben_logo').on('click', function(){
+	// Clicking anywhere on the screen will deactivate the cards.
+	$(document).on('click', function() {
 		$('.oe_card').removeClass('active');
-		$(this).parent('.oe_card').toggleClass('active');
 	});
 
+	// Clicking the card logo will activate the card.
+	$('.oe_card').find('.ben_logo').on('click', function(){
+		event.stopPropagation();
 
+		$('.oe_card').removeClass('active');
+		$(this).parent('.oe_card').toggleClass('active');
+
+	});
+
+	// Click the "More" link will activate the card.
+	$('.seemore').on('click', function() {
+		event.stopPropagation();
+
+		$('.oe_card').removeClass('active');
+		$(this).parent('.oe_card').toggleClass('active');
+		
+	});
 });
